00e69322fbc0ab67ec3c6445719b50f43452ea8b
Ikramovdev <mubinullakh@gmail.com>
2024-09-29 23:37:25 +0500

    firdavsga update

--- src/assets/navbarItem.jsx
+++ src/assets/navbarItem.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 const NavbarItem = ({item}) => {
   return (
         <li>
-            <a className='font-regular text-[16px] text-[#161616] leading-[20px]' href={item.path}>{item.title}</a>
+            <a className='font-regular text-[16px] text-[#161616] leading-[20px] hover:text-[#154444] duration-300' href={item.path}>{item.title}</a>
         </li>
   )
 }
 
 export default NavbarItem
+
+
